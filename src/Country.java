eaecb33af98f60976c3efc4278f00dccbdee2725
SavoB92 <savobozovic92@gmail.com>
2023-04-04 09:53:44 -0400

    First commit with Mili

---
+++ src/Country.java
@@ -0,0 +1,8 @@
+public class Country {
+
+    String country;
+
+    String state;
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+    Country ct = new Country();
+    ct.country = "United States";
+    ct.state = "Florida";
+
+    }
+}
